bdef4040526675759ffbcb482d865334a99d222a
devjinx <t.chareonlertkamol@gmail.com>
2023-09-26 14:51:21 +0700

    update login and register page

--- src/router/login/login.js
+++ src/router/login/login.js
@@ -1,49 +1,78 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
+import './login.css';
 
 function Login() {
-    // State to store user input
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate(); // Initialize navigate for navigation
 
-    // Event handler for form submission
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        // Here, you can add code to send a request to your authentication API
-        // to verify the user's credentials.
-        // For simplicity, we'll just display the input values in this example.
-        console.log('Email:', email);
+        console.log('Username:', username);
         console.log('Password:', password);
     };
 
+    const handleRegisterClick = () => {
+        // Redirect to the registration page
+        navigate('/register');
+    };
+
+    const handleResetpasswordClick = () => {
+        // Redirect to the Reset Password page
+        navigate('/');
+    };
+
     return (
-        <div className="container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="show-password-button"
+                        >
+                            {showPassword ? 'Hide Password' : 'Show Password'}
+                        </button>
+                    </div>
+                    <div className="form-actions">
+                        <button type="button" className="reset-password-button" onClick={handleResetpasswordClick}>
+                            Reset Password
+                        </button>
+                        <button type="button" className="register-button" onClick={handleRegisterClick}>
+                            Register
+                        </button>
+                    </div>
+                    <div className="form-actions">
+                        <button type="submit" className="login-button">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
--- src/router/register/register.js
+++ src/router/register/register.js
@@ -1,66 +1,94 @@
-// src/components/RegistrationForm.js
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function RegistrationForm() {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-    });
+function Registration() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+    const isEmailValid = (email) => {
+        // Regular expression to check for a valid email format
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailRegex.test(email);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Here, you can perform actions like sending the registration data to your server or validating the form.
-        console.log('Form submitted with data:', formData);
+
+        // Check if the entered email is valid
+        if (!isEmailValid(email)) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+
+        console.log('Username:', username);
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    const handleLoginClick = () => {
+        navigate('/login');
     };
 
     return (
-        <div>
-            <h2>Register</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="username"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <button type="submit">Register</button>
-                </div>
-            </form>
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Register</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="show-password-button"
+                        >
+                            {showPassword ? 'Hide Password' : 'Show Password'}
+                        </button>
+                    </div>
+                    <div className="form-actions">
+                        <button type="button" className="login-button" onClick={handleLoginClick}>
+                            Login
+                        </button>
+                        <button type="submit" className="register-button">
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
 
-export default RegistrationForm;
+export default Registration;
